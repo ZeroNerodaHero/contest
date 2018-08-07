97691d9d40baf0944b5a4b1d16fdeb97adec1ffe
Billy <billyz404meter@gmail.com>
2018-08-06 17:57:08 -0700

    remove .in .out file

--- bronze/2017-12/silvermeasure/measure.cpp
+++ bronze/2017-12/silvermeasure/measure.cpp
@@ -17,25 +17,8 @@ int main(){
     for(int i = 0; i < change; i++){
         int d,id,gal;
         in >> d >> id >> gal;
-        day[d][id] = gal;
+        day[d][d] = gal;
     }
 
-    
-
-
-    change = 0;
-    for(int i = 0; i < 3; i++) milk[0][i] = 7;
-    for(int i = 1; i <= 100; i++){
-        milk[i][0] += milk[i-1][0];
-        milk[i][1] += milk[i-1][1];
-        milk[i][2] += milk[i-1][2];
-        
-        if(diff(milk[i],milk[i-1])){
-            change++;
-        }
-    }
-    //if(!diff(milk[99],milk[98])) change--;
-    cout << change << '\n';
-    out << change << '\n';
 }
 
