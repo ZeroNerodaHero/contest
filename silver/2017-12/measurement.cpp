ee5f7d1eedf062e6166549377ea03bbb3119362c
Billy <billyz404meter@gmail.com>
2018-08-07 22:29:27 -0700

    edit measure

--- silver/2017-12/measurement.cpp
+++ silver/2017-12/measurement.cpp
@@ -1,38 +1,89 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <vector>
 #include <map>
 using namespace std;
+
+struct record{
+    int day, id, change;
+    bool operator < (const record &o) const{
+        return day < o.day;
+    }
+};
+
+void print(std::vector<record> &v){
+    for(int i = 0; i < v.size(); i++){
+        std::cout << v[i].day << ' ' << v[i].id<< ' '<< v[i].change << '\n';
+    } 
+}
 
 int main(){
     std::ifstream in("measurement.in");
     std::ofstream out("measurement.out");
 
-    std::map<int,int> daycount;
+    std::map<int,int> galcount;
     std::map<int,int> idgallons;
-    std::map<int,int> curgallons;
-    
-    std::map<int,int> daypts;
-    
 
     int size, gallon;
     in >> size >> gallon;
+    std::vector<record> all(size);
+
     for(int i = 0; i < size; i++){
-        int day;
-        in >> day;
-        in >> dayid[day];
-        curgallons[dayid[day] = gallon;
-        in >> idgallons[daychange[day]];
+        in >> all[i].day >> all[i].id >> all[i].change;
     }
+    std::sort(all.begin(), all.end());
 
-    daypts[0] = gallon;
-    for(std::map<int,int>::iterator a = daychange.begin()+1; a != daychange.end(); a++){
-        if(idgallons[a->first] < 0){
-            if(){
-                
-            }        
+//print(all);
+
+    int change = 0;
+    galcount[0] = size+2;
+    for(int i = 0; i < size; i++){
+        std::map<int,int>::reverse_iterator rit = galcount.rbegin();
+        int id = all[i].id; 
+
+        bool del;        
+        bool wastop = (idgallons[id]== rit->first);
+        if(galcount[idgallons[id]] == 1){
+            galcount.erase(idgallons[id]);
+            del = true;
+        } else{
+            galcount[idgallons[id]]--;
+            del = false;
         }
-        if(idgallons[a->first] > 0}{
 
-        }       
+        idgallons[id] += all[i].change;
+        galcount[idgallons[id]]++;
+        rit = galcount.rbegin(); 
+        bool istop = (idgallons[id] == rit->first);
+
+#if 1
+        if(istop){
+            if(wastop){
+                if(del){
+                    if(galcount[idgallons[id]] > 1)
+                        change++;    
+                } else{
+                    change++;
+                }
+            } else{
+                change++;
+            }
+        } else{
+            if(wastop) change++;
+        }
+#else
+        if(wastop) {
+            if(istop) {
+                if(!del || galcount[idgallons[id]] > 1) change++;
+            } else {
+                change++;
+            }
+        } else {
+            if (istop) change++;
+        }
+#endif
     }
+    std::cout << change << std::endl;
+    out << change << '\n';
 }
